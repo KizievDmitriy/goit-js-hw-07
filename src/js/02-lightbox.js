562433e18f748faa998f62f75ffc9b82ae38d510
KizievDmitriy <kizievdmitrii@gmail.com>
2022-04-27 12:40:39 +0300

    render galleryItems

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItems = ({preview, original, description}) => {
+    const galleryEl = document.createElement("div");
+    galleryEl.insertAdjacentHTML("afterbegin",
+    `<a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+    </a>`);
+  
+  return galleryEl;
+}
+
+const galleryElements = galleryItems.map(createGalleryItems);
+galleryList.append(...galleryElements);
